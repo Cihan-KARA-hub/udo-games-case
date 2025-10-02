c771e536f7dbe79772271eba1d833072f391fc4d
Cihan-KARA-hub <cihankara632@gmail.com>
2025-10-03 02:07:38 +0300

    spring project init and docker compose up

---
+++ src/test/java/com/UDO/GameAnalytics/GameAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UDO.GameAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
